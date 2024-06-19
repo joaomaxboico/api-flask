8514e90fba91e082f0bca0bd9d27f654f08a1241
joaomaxboico <joo_max@yahoo.com.br>
2024-06-19 11:28:11 -0300

    Aceita requisição post, retorna json

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask  # Importação do Flask
+# from flask import Flask  # Importação do Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)  # Cria instância (aplicação flask)
 
 
-# Define rota, qd se mapeia pasta pelo get, chama função com o retorno.
-@app.route("/sorter/", methods=["GET"])
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Define rota, qd se mapeia pasta pelo POST, chama função com o retorno.
+@app.route("/sorter/", methods=["POST"])
+def retorna_json():
+    # Exemplo de resposta JSON
+    data = request.get_json()  # Obtém dados JSON da requisição
+    response = {
+        "message": "Recebido com sucesso",
+        "data": data
+    }
+    return jsonify(response)  # Retorna a resposta JSON
 
 
 if __name__ == "__main__":  # Executa a aplicação flask
-    app.run()  # inicia a aplicação na porta 8008
-# teste git novo
+    app.run()
 
-    # flask run --port 8008 ---Executar rodando em outra porta
+    # Executar rodando em outra porta:
+    # flask run --port 8008
+
+    # Postman localhost:8008/sorter
